cb1249cb8c56cc45d11465886b8a84381779b85c
Mart <martleonarberido@gmail.com>
2023-08-05 18:29:44 -0700

    First Commit - Pawsome Companions

---
+++ pawsome/src/test/javaScript/payment/payment.test.js
@@ -0,0 +1,40 @@
+// npm install --save-dev jest - if some modules are missing
+// import all missing files
+// to run a test use: npx jest payment.test.js
+
+const { validateForm } = require('./payment.js');
+const { JSDOM } = require('jsdom');
+const { document } = new JSDOM('').window;
+
+global.document = document;
+global.alert = jest.fn();
+
+test('Return true if the form is valid', () => {
+
+    document.body.innerHTML = `
+    <form onsubmit="return validateForm()">
+      <input type="text" id="creditCardNumber" value="1234123412341234">
+      <input type="text" id="expirationMonth" value="05">
+      <input type="text" id="expirationYear" value="2025">
+      <input type="text" id="cvc" value="123">
+      <input type="submit" value="Submit">
+    </form>
+  `;
+
+    expect(validateForm()).toBe(true);
+});
+
+test('Return true if the form is invalid', () => {
+
+    document.body.innerHTML = `
+    <form onsubmit="return validateForm()">
+      <input type="text" id="creditCardNumber" value="1234"> 
+      <input type="text" id="expirationMonth" value="13"> 
+      <input type="text" id="expirationYear" value="20">
+      <input type="text" id="cvc" value="12"> 
+      <input type="submit" value="Submit">
+    </form>
+  `;
+
+    expect(validateForm()).toBe(false);
+});
